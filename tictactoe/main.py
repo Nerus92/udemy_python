c3b3733ac83ae37d642193c7c48c422a4a13418d
Nerus92 <jphilippe.chatain@gmail.com>
2016-01-28 22:40:45 +0100

    Completed game main loop condition - Added base for invalid inputs checking - Added turn taking Player1/2

--- tictactoe/main.py
+++ tictactoe/main.py
@@ -5,10 +5,20 @@ class TicTacToe(object):
         print 'Welcome to TicTacToe!'
         print 'Here is the grid'
         self.print_grid()
-        while self.no_winner() and self.grid_not_full():
+        while self.game_continues():
+            print 'Alright Player {}, your turn:'.format(self.player_turn)
             input = raw_input()
             if input == "quit":
+                self.user_requested_exit = True
                 break
+            if self.invalid_move(input):
+                print 'Sorry, you cannot play this'
+                continue
+
+            self.change_player_turn()
+
+    def game_continues(self):
+        return self.no_winner() and self.grid_not_full() and self.user_wants_to_continue()
 
     def print_grid(self):
         counter = 0
@@ -18,8 +28,20 @@ class TicTacToe(object):
                 self.print_line_separation()
             counter += 1
 
+    def invalid_move(self, input):
+        return True
+
+    def change_player_turn(self):
+        if self.player_turn == 1:
+            self.player_turn = 2
+        else:
+            self.player_turn == 1
+
     def no_winner(self):
         return True
+
+    def user_wants_to_continue(self):
+        return not self.user_requested_exit
 
     def grid_not_full(self):
         for line in self.grid:
@@ -35,6 +57,8 @@ class TicTacToe(object):
         print "___________"
 
     def init_game(self):
+        self.user_requested_exit = False
+        self.player_turn = 1
         self.grid = 3 * [3 * [" "]]
         self.player1_marker = "X"
         self.player2_marker = "O"
